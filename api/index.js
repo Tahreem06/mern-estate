5f2d0d6d05b53925191841e532702f04f0026843
Tahreem <tahreemmahboob06@gmail.com>
2026-04-06 01:50:45 +0500

    Create signup api route

--- api/index.js
+++ api/index.js
@@ -12,8 +12,11 @@ mongoose
     console.log(err);
   });
 
-  
 const app = express();
+
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
+app.use('/api/auth', authRouter);
