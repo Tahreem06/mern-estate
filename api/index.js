b0bcbcfa49953849b9db33157bcb57520b006f31
Tahreem <tahreemmahboob06@gmail.com>
2026-04-06 02:03:36 +0500

    create a middleware and a function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -19,4 +19,15 @@ app.use(express.json());
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
+
 app.use('/api/auth', authRouter);
+
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong!";
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+    });
+})
