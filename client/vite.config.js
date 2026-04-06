08879d46bf1a76975417350e2b4a02a43b65b504
Tahreem <tahreemmahboob06@gmail.com>
2026-04-06 19:48:35 +0500

    complete signup page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server:{
+    proxy:{
+      '/api': {
+        target:'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
   ],
 })
