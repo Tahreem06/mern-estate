0401096a743f24990100eaa67f9c0de0a7e8bc01
Tahreem <tahreemmahboob06@gmail.com>
2026-04-12 01:48:08 +0500

    Add sign out functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -8,6 +8,9 @@ import {
   deleteUserFailure,
   deleteUserSuccess,
   deleteUserStart,
+  signOutUserStart,
+  signOutUserSuccess,
+  signOutUserFailure,
 } from "../redux/user/userSlice.js";
 
 export default function Profile() {
@@ -110,6 +113,21 @@ const handleDeleteUser = async () => {
     }
   };
 
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart())
+      const res = await fetch("/api/auth/signout");
+      const data = await res.json();
+      if(data.success === false) {
+        dispatch(signOutUserFailure(data.message));
+        return;
+      }
+      dispatch(signOutUserSuccess(null));
+    } catch (error) {
+      dispatch(signOutUserFailure(error.message));
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -173,7 +191,7 @@ const handleDeleteUser = async () => {
 
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete Account</span>
-        <span className="text-red-700 cursor-pointer">Sign out</span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Sign out</span>
       </div>
       <p  className="text-red-700 mt-5">
         {error ? error : ""}
