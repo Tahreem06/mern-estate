e3620ff334459dcd67a2c37d65db05a8dde6ca67
Tahreem <tahreemmahboob06@gmail.com>
2026-04-11 09:21:26 +0500

    errors

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,9 +16,9 @@ const userSchema = new mongoose.Schema(
         type: String,
         required: true,
     }, 
-    avator:{
+    avatar:{
         type: String,
-        default:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+        default:"https://cdn-icons-png.flaticon.com/512/149/149071.png"
     },
 }, {timestamps: true});
 
