26cc38fbed40866c768e3976575501530c7ab4cb
Tahreem <tahreemmahboob06@gmail.com>
2026-04-15 19:20:29 +0500

    complete About Page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,11 @@ import React from "react";
 
 export default function About() {
     return(
-        <div>About</div>
+        <div className="py-20 px-4 max-w-6xl mx-auto">
+            <h1 className="text-3xl font-bold mb-4 text-slate-800">About MJ Estate</h1>
+            <p className="text-slate-700 mb-4">MJ Estate is a leading real estate company dedicated to providing exceptional service and quality properties to our clients. </p>
+            <p className="text-slate-700 mb-4">We have been serving the real estate market for over a decade, helping clients find their perfect homes and investments.</p>
+            <p className="text-slate-700 mb-4">Our team of experienced professionals is committed to delivering personalized solutions and ensuring a seamless real estate experience for our clients.</p>
+        </div>
     )
 }
